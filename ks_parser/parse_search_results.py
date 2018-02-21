98cdd8831bf049301411ddf549e69f980162963b
Saša Prole <proles@comtrade.com>
2018-02-21 21:05:22 +0100

    Added initial KickStarterProject and  initial parsing of projects (id parsing)

--- ks_parser/parse_search_results.py
+++ ks_parser/parse_search_results.py
@@ -1,8 +1,16 @@
 from bs4 import BeautifulSoup
-
+from .KickstarterProject import KickstarterProject
+import json
 class ProjectsFromResultsParser():
 
     def parse(self, result):
         soup = BeautifulSoup(result, 'html.parser')
-        data = soup.findAll('div')
-        return data
+        divs = soup.findAll('div')
+
+        projects = []
+        for div in divs:
+            if div.get('data-project'):
+                d = json.loads(div.get('data-project'))
+                ksp = KickstarterProject(d['id'])
+                projects.append(ksp)
+        return projects
--- start.py
+++ start.py
@@ -1,17 +1,20 @@
 import time
 import random
 from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
 from ks_fetcher.fetch_projects import KickstarterProjectsFetcher
 from ks_parser.parse_search_results import ProjectsFromResultsParser
+import sys
 kf = KickstarterProjectsFetcher(1,50)
 
 
 client = MongoClient()
 ks_db = client.kickstarter
 ks_searches = ks_db.searches
+ks_projects = ks_db.projects
 
 def fetch_data(from_page,to_page):
-    for i in range(from_page,to_page):
+    for i in range(from_page, to_page):
         print(i)
         result, url_to_fetch = kf.fetch_next()
         entry = {
@@ -24,10 +27,21 @@ def fetch_data(from_page,to_page):
 
 def parse_data():
     parser = ProjectsFromResultsParser()
+    i = 0
     for entry in ks_searches.find():
         try:
-            print(parser.parse(entry['result']))
+            # if i > 1:
+            #     break
+            i += 1
+            projects = parser.parse(entry['result'])
+            for project in projects:
+                try:
+                    entry_id = ks_projects.insert_one(dict(project)).inserted_id
+                except DuplicateKeyError:
+                    print('Key already exists')
+            #ile_to_write = open('file'+str(i)+'.txt', 'w')
+            #file_to_write.write(projects[0])
         except:
-            print("error while parsing")
+            print("error while parsing", sys.exc_info()[0])
 
 parse_data()
